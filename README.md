332cb259623d185b3970d2a287bae1b39d887040
alcereo <ak-91@mail.ru>
2018-05-05 10:10:25 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Benchmark gradle jmh sample
+
+Sample with:
+- `jmh-gradle-plugin:0.4.5`
+- `jmh-core:1.21`
+
+Best full samples pack at: https://github.com/melix/jmh-gradle-example
---
+++ src/jmh/java/com/github/alcereo/benchtest/Application.java
@@ -0,0 +1,74 @@
+package com.github.alcereo.benchtest;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.BenchmarkParams;
+import org.openjdk.jmh.infra.ThreadParams;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@State(Scope.Benchmark)
+public class Application {
+
+    private static int count = 1000000;
+
+    private ConcurrentHashMap<String, String> testedMap;
+
+    @Setup
+    public void setup(BenchmarkParams params){
+        testedMap = new ConcurrentHashMap<>(
+                count,
+                0.75f,
+                1
+        );
+    }
+
+
+    @State(Scope.Thread)
+    public static class Keygen {
+        private String[] keys;
+
+        @Setup
+        public void setup(ThreadParams threads) {
+
+            keys = new String[count];
+            for (int i = 0; i < count; i++) {
+                System.out.printf("\r - %d", i);
+                keys[i] = UUID.randomUUID().toString();
+            }
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @Threads(4)
+    @Warmup(
+            iterations = 1
+    )
+    @Measurement(
+            iterations = 1
+    )
+    public void benchTest(Keygen keys) {
+        for (String key : keys.keys) {
+            testedMap.remove(key);
+            testedMap.put(key, key);
+        }
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(".*" + Application.class.getSimpleName() + ".*")
+//                .mode(Mode.All)
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
